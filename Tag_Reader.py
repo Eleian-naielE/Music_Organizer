89436a73cac2c9f7ace0b329fb8acf0cb1fd3fba
SFGeekFromK <swjacklee@gmail.com>
2024-07-30 00:54:52 +0900

    Tag_Reader.py 4

--- Tag_Reader.py
+++ Tag_Reader.py
@@ -7,5 +7,17 @@ items = os.listdir(cdir)
 
 Files = [f for f in items if os.path.isfile(cdir+'/'+f)]
 
+artist_Dict={}
+
 for file in Files :
-    tnytg.get(file)
+    tag=tnytg.get(file)
+    artist_info=tag.artist
+    
+    if artist_info in artist_Dict :
+        artist_Dict.get(artist_info).append(file)
+    else : 
+        artist_Dict[artist_info] = [file,]
+
+
+#fix indentation erorr
+#for folderCheck in artist_Dict:
