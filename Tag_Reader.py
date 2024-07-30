7b0672d624cb8c7e21aadb0069ef884237ee0aa3
SFGeekFromK <swjacklee@gmail.com>
2024-07-30 20:17:14 +0900

    Tag_Reader.py 6

--- Tag_Reader.py
+++ Tag_Reader.py
@@ -21,14 +21,16 @@ for file in Files :
     else : 
         artist_Dict[artist_info] = [file,]
 
-folderNames = artist_Dict.Keys()
+folderNames = artist_Dict.keys()
 
 #fix indentation erorr (?)
-# this loop should move files from playlist to folder named as artists.
+# this loop should move files from playlist to folder named as artists if the artist has more than 3 tracks
 for folderCheck in folderNames:
-  if len(artist_Dict[folderCheck]) >= 3 :
-    os.mkdir(cdir+'/'+folderCheck)
-    su.
-  else:
-    os.mkdir(other_dir+'/'+folderCheck)
-    
+    if len(artist_Dict[folderCheck]) >= 3 :
+      artist_dir=os.mkdir(cdir+'/'+folderCheck)
+      for src_mvd in artist_Dict[folderCheck]:
+         su.move(src_mvd, dst_dir)
+    else:
+      #other_subdir=os.mkdir(other_dir)
+      for src_mvd in artist_Dict[folderCheck]:
+         su.move(src_mvd, other_dir)     
